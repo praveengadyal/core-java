1db12887f2c1bff4c3929f811d0980d4d707affb
praveengadyal <praveengadyal.xworkz@gmail.com>
2022-06-21 10:58:13 +0530

    committing file

---
+++ PrimeVideos.java
@@ -0,0 +1,185 @@
+class PrimeVideos{
+
+public static void main(String args[]){
+
+      String series="panchayat";
+      float rating = 8.8f;
+      int release = 2020; 
+      System.out.println(series);
+      System.out.println(rating);
+      System.out.println(release);
+ 
+     String series1 ="modern love:mumbai";
+     float rating1 =9.2f;
+     int release1  =2022;
+     System.out.println(series1);
+     System.out.println(rating1);
+     System.out.println(release1);
+
+      String series2 ="guilty minds";
+     float rating2= 8.6f;
+     int release2= 2022;
+     System.out.println(series2);
+     System.out.println(rating2);
+     System.out.println(release2);
+
+     String series3="bestseller";
+      float rating3 = 7.1f;
+      int release3 = 2022; 
+      System.out.println(series3);
+      System.out.println(rating3);
+      System.out.println(release3);
+
+      String series4="solos";
+      float rating4 = 9.5f;
+      int release4 = 2021; 
+      System.out.println(series4);
+      System.out.println(rating4);
+      System.out.println(release4);
+
+        String series5="star treck:picard";
+      float rating5 = 7.3f;
+      int release5 = 2022; 
+      System.out.println(series5);
+      System.out.println(rating5);
+      System.out.println(release5);
+
+       String series6="reacher";
+      float rating6 = 8.9f;
+      int release6 = 2022; 
+      System.out.println(series6);
+      System.out.println(rating6);
+      System.out.println(release6);
+
+           String series7="unpaused :naya safar";
+      float rating7 = 7.2f;
+      int release7 = 2022; 
+      System.out.println(series7);
+      System.out.println(rating7);
+      System.out.println(release7);
+
+             String series8="inside edge:3";
+      float rating8 = 8.1f;
+      int release8 = 2021; 
+      System.out.println(series8);
+      System.out.println(rating8);
+      System.out.println(release8);
+
+           String series9="the wheel of time";
+      float rating9 = 7.4f;
+      int release9 = 2021; 
+      System.out.println(series9);
+      System.out.println(rating9);
+      System.out.println(release9);
+
+    String series10 ="mumbai diaries 26/11";
+     float rating10 =7.9f;
+     int release10  =2021;
+     System.out.println(series10);
+     System.out.println(rating10);
+     System.out.println(release10);
+
+     String series11 ="the family man";
+     float rating11 =8.6f;
+     int release11  =2019;
+     System.out.println(series11);
+     System.out.println(rating11);
+     System.out.println(release11);
+
+       String movie ="pushpa";
+      float movieRating = 8.6f;
+      int movieRelease = 2022;
+     System.out.println(movie);
+     System.out.println(movieRating);
+     System.out.println(movieRelease);
+
+        String movie1 ="gehraiyaan";
+      float movieRating1 = 7.6f;
+      int movieRelease1 = 2022;
+     System.out.println(movie1);
+     System.out.println(movieRating1);
+     System.out.println(movieRelease1);
+
+      
+       String movie2 ="jodi";
+      float movieRating2 =5.3f;
+      int movieRelease2 = 2021;
+     System.out.println(movie2);
+     System.out.println(movieRating2);
+     System.out.println(movieRelease2);
+
+     String movie3 ="mahaan";
+      float movieRating3 = 8.0f;
+      int movieRelease3= 2022;
+     System.out.println(movie3);
+     System.out.println(movieRating3);
+     System.out.println(movieRelease3);
+
+       String movie4 ="encounter";
+      float movieRating4 = 5.8f;
+      int movieRelease4 = 2021;
+     System.out.println(movie4);
+     System.out.println(movieRating4);
+     System.out.println(movieRelease4);
+     
+       String movie5 ="dybbuk";
+      float movieRating5 = 7.0f;
+      int movieRelease5 = 2021;
+     System.out.println(movie5);
+     System.out.println(movieRating5);
+     System.out.println(movieRelease5);
+
+     String movie6 ="jai bhim";
+      float movieRating6 = 8.6f;
+      int movieRelease6 = 2021;
+     System.out.println(movie6);
+     System.out.println(movieRating6);
+     System.out.println(movieRelease6);
+
+       String movie7 ="sardar udham";
+      float movieRating7 = 9.8f;
+      int movieRelease7 = 2021;
+     System.out.println(movie7);
+     System.out.println(movieRating7);
+     System.out.println(movieRelease7);
+
+       String movie8 ="bhramam";
+      float movieRating8 = 6.3f;
+      int movieRelease8 = 2021;
+     System.out.println(movie8);
+     System.out.println(movieRating8);
+     System.out.println(movieRelease8);
+
+       String movie9 ="v";
+      float movieRating9 = 6.8f;
+      int movieRelease9 = 2021;
+     System.out.println(movie9);
+     System.out.println(movieRating9);
+     System.out.println(movieRelease9);
+
+     String movie10 ="soorarai pottru";
+      float movieRating10 = 9.3f;
+      int movieRelease10 = 2021;
+     System.out.println(movie10);
+     System.out.println(movieRating10);
+     System.out.println(movieRelease10);
+     
+    String movie11 ="roberrt";
+      float movieRating11 = 7.2f;
+      int movieRelease11 = 2021;
+     System.out.println(movie11);
+     System.out.println(movieRating11);
+     System.out.println(movieRelease11);
+
+    String movie12 ="maara";
+      float movieRating12 = 7.7f;
+      int movieRelease12 = 2021;
+     System.out.println(movie12);
+     System.out.println(movieRating12);
+     System.out.println(movieRelease12);
+         
+    
+}
+
+
+}
---
+++ Zee5.java
@@ -0,0 +1,185 @@
+class Zee5{
+
+public static void main(String args[]){
+
+      String series="bloody brothers";
+      float rating = 8.0f;
+      int release = 2022; 
+      System.out.println(series);
+      System.out.println(rating);
+      System.out.println(release);
+ 
+     String series1 ="abhay:3";
+     float rating1 =8.1f;
+     int release1  =2022;
+     System.out.println(series1);
+     System.out.println(rating1);
+     System.out.println(release1);
+
+      String series2 ="sutliyan";
+     float rating2= 8.0f;
+     int release2= 2022;
+     System.out.println(series2);
+     System.out.println(rating2);
+     System.out.println(release2);
+
+     String series3="mithya";
+      float rating3 = 7.6f;
+      int release3 = 2022; 
+      System.out.println(series3);
+      System.out.println(rating3);
+      System.out.println(release3);
+
+      String series4="kaun banegi shikharwati";
+      float rating4 = 6.4f;
+      int release4 = 2022; 
+      System.out.println(series4);
+      System.out.println(rating4);
+      System.out.println(release4);
+
+        String series5="engineering girls:2";
+      float rating5 = 6.9f;
+      int release5 = 2021; 
+      System.out.println(series5);
+      System.out.println(rating5);
+      System.out.println(release5);
+
+       String series6="sunflower";
+      float rating6 = 7.4f;
+      int release6 = 2021; 
+      System.out.println(series6);
+      System.out.println(rating6);
+      System.out.println(release6);
+
+           String series7="jeet ki zid";
+      float rating7 = 8.3f;
+      int release7 = 2021; 
+      System.out.println(series7);
+      System.out.println(rating7);
+      System.out.println(release7);
+
+             String series8="black widows";
+      float rating8 = 9.8f;
+      int release8 = 2020; 
+      System.out.println(series8);
+      System.out.println(rating8);
+      System.out.println(release8);
+
+           String series9="poison:2";
+      float rating9 = 6.5f;
+      int release9 = 2020; 
+      System.out.println(series9);
+      System.out.println(rating9);
+      System.out.println(release9);
+
+    String series10 ="taish";
+     float rating10 =8.6f;
+     int release10  =2020;
+     System.out.println(series10);
+     System.out.println(rating10);
+     System.out.println(release10);
+
+     String series11 ="the final call";
+     float rating11 =7.3f;
+     int release11  =2019;
+     System.out.println(series11);
+     System.out.println(rating11);
+     System.out.println(release11);
+
+       String movie ="valimai";
+      float movieRating = 6.6f;
+      int movieRelease = 2022;
+     System.out.println(movie);
+     System.out.println(movieRating);
+     System.out.println(movieRelease);
+
+        String movie1 ="love hostel";
+      float movieRating1 = 8.0f;
+      int movieRelease1 = 2022;
+     System.out.println(movie1);
+     System.out.println(movieRating1);
+     System.out.println(movieRelease1);
+
+      
+       String movie2 ="bob biswas";
+      float movieRating2 =7.5f;
+      int movieRelease2 = 2021;
+     System.out.println(movie2);
+     System.out.println(movieRating2);
+     System.out.println(movieRelease2);
+
+     String movie3 ="qismat :2";
+      float movieRating3 = 7.6f;
+      int movieRelease3= 2021;
+     System.out.println(movie3);
+     System.out.println(movieRating3);
+     System.out.println(movieRelease3);
+
+       String movie4 ="squad";
+      float movieRating4 = 7.1f;
+      int movieRelease4 = 2021;
+     System.out.println(movie4);
+     System.out.println(movieRating4);
+     System.out.println(movieRelease4);
+     
+       String movie5 ="helmet";
+      float movieRating5 = 7.0f;
+      int movieRelease5 = 2021;
+     System.out.println(movie5);
+     System.out.println(movieRating5);
+     System.out.println(movieRelease5);
+
+     String movie6 ="dial 100";
+      float movieRating6 = 9.1f;
+      int movieRelease6 = 2021;
+     System.out.println(movie6);
+     System.out.println(movieRating6);
+     System.out.println(movieRelease6);
+
+       String movie7 ="state of siege:temple attack";
+      float movieRating7 = 8.1f;
+      int movieRelease7 = 2021;
+     System.out.println(movie7);
+     System.out.println(movieRating7);
+     System.out.println(movieRelease7);
+
+       String movie8 ="the power";
+      float movieRating8 = 7.1f;
+      int movieRelease8 = 2021;
+     System.out.println(movie8);
+     System.out.println(movieRating8);
+     System.out.println(movieRelease8);
+
+       String movie9 ="kaagaz";
+      float movieRating9 = 7.3f;
+      int movieRelease9 = 2021;
+     System.out.println(movie9);
+     System.out.println(movieRating9);
+     System.out.println(movieRelease9);
+
+     String movie10 ="solo brathuke so better";
+      float movieRating10 = 6.8f;
+      int movieRelease10 = 2021;
+     System.out.println(movie10);
+     System.out.println(movieRating10);
+     System.out.println(movieRelease10);
+     
+    String movie11 ="darbaan";
+      float movieRating11 = 8.0f;
+      int movieRelease11 = 2020;
+     System.out.println(movie11);
+     System.out.println(movieRating11);
+     System.out.println(movieRelease11);
+
+    String movie12 ="pareeksha";
+      float movieRating12 = 8.1f;
+      int movieRelease12 = 2020;
+     System.out.println(movie12);
+     System.out.println(movieRating12);
+     System.out.println(movieRelease12);
+         
+    
+}
+
+
+}
