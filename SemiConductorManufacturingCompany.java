8ae5b6fc7fe601ee22bf42bb928db70da1df86be
praveengadyal <praveengadyal.xworkz@gmail.com>
2022-06-25 15:11:51 +0530

    committing file

---
+++ SemiConductorManufacturingCompany.java
@@ -0,0 +1,23 @@
+class SemiConductorManufacturingCompany{
+
+
+int founded;
+String headQuarters;
+String founder;
+String type;
+int noOfEmployees;
+String revenue;
+
+public void provideCompactness(){
+System.out.println("provide compactness");
+}
+
+public void provideReliability(){
+System.out.println("provides reliability");
+}
+
+public void powerEfficiency(){
+System.out.println("provide power efficiency");
+}
+
+}
